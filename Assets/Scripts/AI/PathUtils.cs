b7e84deceec719e09c974500a1d8741a19227fef
Walter Palladino <wpalladino@gmail.com>
2020-06-22 19:26:25 -0300

    Added navigation basis

---
+++ Assets/Scripts/AI/PathUtils.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class PathUtils {
+
+
+    public static Vector3 CheckEndPosition (Vector3 startPosition, Vector3 testPosition, LayerMask layerMask, float maxDistance) {
+
+        float distance = 0;
+        Vector3 endPosition = Vector3.zero;
+
+        NavMeshPath path = new NavMeshPath();
+        if (NavMesh.CalculatePath(startPosition, testPosition, layerMask, path))
+        {
+            endPosition = testPosition;
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                float segmentDistance = (path.corners[i + 1] - path.corners[i]).magnitude;
+                if (distance + segmentDistance <= maxDistance)
+                {
+                    distance += segmentDistance;
+                }
+                else // Path length exceeds maxDistance
+                {
+                    endPosition = path.corners[i] + ((path.corners[i + 1] - path.corners[i]).normalized * (maxDistance - distance));
+                    break;
+                }
+            }
+
+        } else {
+            Debug.Log("No Path found");
+        }
+
+        return endPosition;
+    }
+
+    public static Vector3 CheckEndPosition(NavMeshAgent agent, Vector3 testPosition, float maxDistance)
+    {
+
+        float distance = 0;
+        Vector3 endPosition = Vector3.zero;
+
+        NavMeshPath path = new NavMeshPath();
+        if (agent.CalculatePath(testPosition, path))
+        {
+            endPosition = testPosition;
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                float segmentDistance = (path.corners[i + 1] - path.corners[i]).magnitude;
+                if (distance + segmentDistance <= maxDistance)
+                {
+                    distance += segmentDistance;
+                }
+                else // Path length exceeds maxDistance
+                {
+                    endPosition = path.corners[i] + ((path.corners[i + 1] - path.corners[i]).normalized * (maxDistance - distance));
+                    break;
+                }
+            }
+
+        }
+        else
+        {
+            Debug.Log("No Path found");
+        }
+
+        return endPosition;
+    }
+
+    public static float CalculateDistance (Vector3 startPosition, Vector3 endPosition, LayerMask layerMask)
+    {
+        Debug.Log("Crow Distance: " + Vector3.Distance(startPosition, endPosition));
+        float distance = -1;
+
+        NavMeshPath path = new NavMeshPath();
+        if (NavMesh.CalculatePath(startPosition, endPosition, layerMask, path)) {
+            distance = 0;
+            Debug.Log(path.corners.Length);
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                float segmentDistance = (path.corners[i + 1] - path.corners[i]).magnitude;
+                distance += segmentDistance;
+            }
+
+        }
+
+        return distance;
+    }
+
+    public static float CalculateDistance(NavMeshAgent agent, Vector3 endPosition)
+    {
+        float distance = -1;
+
+        NavMeshPath path = new NavMeshPath();
+        if (agent.CalculatePath( endPosition, path))
+        {
+            distance = 0;
+            Debug.Log(path.corners.Length);
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                float segmentDistance = (path.corners[i + 1] - path.corners[i]).magnitude;
+                distance += segmentDistance;
+            }
+
+        }
+
+        return distance;
+    }
+
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask _layerMask;   //  Used to filter which are the walkable areas
+
+
+    private Animator _animator;
+    private NavMeshAgent _navMeshAgent;
+
+    private float _maxDistance = 100.0f;
+
+    private bool _isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_navMeshAgent.enabled)
+        {
+
+            _isMoving = CheckIfMoving();
+
+            if (!_isMoving)
+            {
+
+//                _animator.SetFloat("Speed", 0.0f);
+                _navMeshAgent.isStopped = true;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit hitInfo;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    bool hit = Physics.Raycast(ray, out hitInfo, _maxDistance, _layerMask);
+
+                    //  If we hit something
+                    if (hit)
+                    {
+                        Debug.Log("Hit : " + hitInfo.transform.gameObject.name);
+                        //  Move the character to the destination
+                        Vector3 newPosition = hitInfo.point;
+                        newPosition.y = transform.position.y;   //  Keep the same height
+
+                        Debug.Log("Original newPosition : " + newPosition);
+
+                        //  Check if the destination is reachable on this turn and limit it if its needed
+                        //                  newPosition = PathUtils.CheckEndPosition(_navMeshAgent, newPosition, _characterProfile.movementWalk);
+                        newPosition = PathUtils.CheckEndPosition(_navMeshAgent, newPosition, 100);
+
+                        //                Debug.Log("Updated newPosition : " + newPosition);
+
+                        //  Validate if the destination is reachable
+                        NavMeshPath path = new NavMeshPath();
+                        bool canGetPath = _navMeshAgent.CalculatePath(newPosition, path);
+                        Debug.Log("Can Get Path : " + canGetPath);
+                        Debug.Log(" Path Status : " + path.status);
+
+                        if (path.status == NavMeshPathStatus.PathComplete)
+                        {
+                            //_animator.SetFloat("Speed", _navMeshAgent.speed);
+
+                            _navMeshAgent.updateRotation = true;
+                            _navMeshAgent.isStopped = false;
+                            _navMeshAgent.SetDestination(newPosition);
+
+    //                        _positionMarker.DisplayMarker(newPosition);
+                            _isMoving = true;
+
+  //                          _gameTurnManager.Next();
+                        }
+
+
+                    }
+
+                }
+
+                if (!_isMoving)
+                {
+//                    _positionMarker.HideMarker();
+                }
+
+            }
+        }
+
+    }
+
+
+    //  Check if the model is moving on the NavMesh
+    private bool CheckIfMoving()
+    {
+
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+
+
+}
